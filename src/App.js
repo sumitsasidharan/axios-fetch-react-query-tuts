52a1f96b600bae4221f5f797ffd24da8fb17edb0
sumitsasidharan <s.sumit196@gmail.com>
2022-12-26 21:07:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Title from './components/Title';
+import SetUp from './examples/3-post-request';
+
+function App() {
+  return (
+     <main>
+        <Title />
+        <SetUp />
+     </main>
+  );
+}
+
+export default App;
+// 15.52
