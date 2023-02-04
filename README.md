5d1d9242c56d8c78e12dbaa59d4ecfa1f1911c55
sumitsasidharan <s.sumit196@gmail.com>
2023-02-04 20:38:38 +0530

    add queryclient provider

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## REACT TANSTACK QUERY TUTORIAL
 
-### STEP 1
+### STEP 1: Install json-server
 
 1. Install `json-server`
 2. Add `"serve-json": "json-server --watch db.json --port 4000"` in package.json under "scripts"
@@ -28,4 +28,24 @@
       }
    ]
 }
+```
+
+### STEP 2: React Tanstack Query Setup
+
+1. Import ==QueryClient and QueryClientProvider== from 'react-query' in App.jsx.
+2. Wrap the whole component with QueryClientProvider.
+3. Create an instance of the 'QueryClient':
+
+   `const queryClient = new QueryClient();`
+
+4. Add a =='client'== prop in the ==<QueryClientProvider>== component, and pass the 'queryClient' instance.
+
+   `<QueryClientProvider client={queryClient} >`
+
+App.jsx Code:
+
+```js
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+
 ```
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import HomePage from './components/Home.page';
+import RQSuperHeroesPage from './components/RQSuperHeroes.page';
+import SuperHeroesPage from './components/SuperHeroes.page';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+import './App.css';
+
+const queryClient = new QueryClient();
 
 function App() {
-  
+   return (
+      <QueryClientProvider>
+         <nav>
+            <ul>
+               <li>
+                  <Link to="/">Home</Link>
+               </li>
+               <li>
+                  <Link to="/super-heroes">Traditional Super Heroes</Link>
+               </li>
+               <li>
+                  <Link to="/rq-super-heroes">RQ Super Heroes</Link>
+               </li>
+            </ul>
+         </nav>
+         <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/super-heroes" element={<SuperHeroesPage />} />
+            <Route path="/rq-super-heroes" element={<RQSuperHeroesPage />} />
 
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  )
+            <Route path="*" element={<h1>Not Found</h1>} />
+         </Routes>
+      </QueryClientProvider>
+   );
 }
 
-export default App
+export default App;
